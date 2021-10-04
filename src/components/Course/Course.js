cfc3ff07e7a4cd38f740b3755e6b8089ab083baa
musasazib <musasazib@gmail.com>
2021-10-04 07:58:29 +0600

    Added courses

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import './Course.css';
 
-const Course = () => {
+const Course = (props) => {
+    const { course, picture,  lessons, week, views, name, balance } = props.course;
     return (
-        <div>
-            
+        <div className="courses">
+            <img className="courses-img" src={picture} alt="" />
+            <h5>{course}</h5>
+            <div className="course-detail">
+                <small className="views-color">{views} Views</small>
+                <small className="lessons-color">{lessons} Lessons</small>
+                <small className="week-color">{week} Week</small>
+            </div>
+            <div className="author-price">
+                <p>Author: <span className="author">{name}</span></p>
+                <h6>$ {balance}</h6>
+            </div>
         </div>
     );
 };
--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Course from '../Course/Course';
 import './Courses.css';
 
 const Courses = () => {
+    const [courses, setCourses] = useState([]);
+    useEffect(() => {
+        fetch('./courseData.JSON')
+            .then(res => res.json())
+            .then(data => setCourses(data))
+    }, [])
     return (
         <div>
-            
+            <h1 className="courses-title">Courses</h1>
+            <div className="courses-container">
+                {
+                    courses.map(course => <Course
+                        key={course.id}
+                        course={course}
+                    ></Course>)
+                }
+            </div>
         </div>
     );
 };
